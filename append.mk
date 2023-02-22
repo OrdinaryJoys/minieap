85ad8b9a262ede21bd3209963f401267831f088a
OrdinaryJoys <90075145+OrdinaryJoys@users.noreply.github.com>
2023-02-22 15:21:04 +0800

    Update append.mk

--- append.mk
+++ append.mk
@@ -25,15 +25,12 @@ $($(LOCAL_MODULE)_PRIV_OBJS) : ${@:.o=.c}
 $($(LOCAL_MODULE)_PRIV_DEPS) : LCFLAGS := $($(LOCAL_MODULE)_PRIV_C_FLAGS)
 $($(LOCAL_MODULE)_PRIV_DEPS) : ${@:.d=.c}
 	@$(CC) -MM ${@:.d=.c} $(LCFLAGS) > $@
-	@sed -i -e 's|^.*:|${@:.d=.o} $@ : |g' $@
+	@sed -i 's,^.*:,${@:.d=.o} $@ : ,g' $@
 
 # %.o %.d : *.c
-# This will cause regeneration of .d files and unselected targets.
-# Only include deps when building for selected modules.
-ifneq ("$(MAKECMDGOALS)","clean")
-ifneq ($(filter $(LOCAL_MODULE),$(BUILD_MODULES)),)
+# This will cause regeneration of .d files. Do not do this when cleaning.
+ifneq "$(MAKECMDGOALS)" "clean"
 -include $($(LOCAL_MODULE)_PRIV_DEPS)
-endif
 endif
 
 .PHONY: $(LOCAL_MODULE)_clean
